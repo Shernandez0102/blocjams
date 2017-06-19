9b883c3d68cd89737d76af59e72a9dea779d6abd
Stephanie Hernandez <shernandez@Stephanies-MBP.home>
2017-06-18 21:27:14 -0400

    Assignment 19 Second Attempt

--- scripts/album.js
+++ scripts/album.js
@@ -1,3 +1,17 @@
+var setSong = function(songNumber) {
+    currentlyPlayingSongNumber = parseInt (songNumber);
+    currentSongFromAlbum = currentAlbum.song[songNumber - 1];
+
+
+};
+
+var getSongNumberCell = function (number) {
+  return $('.song-item-number[data-song-number="' + number + '"]')
+}
+
+
+
+
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -16,7 +30,7 @@
           if (currentlyPlayingSongNumber !== null) {
              // Revert to song number for currently playing song because user started playing new song.
 
-                 var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+                 var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
 
                  currentlyPlayingCell.html(currentlyPlayingSongNumber);
          }
@@ -25,8 +39,7 @@
         if (currentlyPlayingSongNumber !== songNumber) {
              // Switch from Play -> Pause button to indicate new song is playing.
              $(this).html(pauseButtonTemplate);
-            currentlyPlayingSongNumber = songNumber;
-            currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+            setSong(songNumber);
             updatePlayerBarSong();
         } else if (currentlyPlayingSongNumber === songNumber) {
              // Switch from Pause -> Play button to pause currently playing song.
